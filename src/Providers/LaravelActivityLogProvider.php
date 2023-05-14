12280029b6416eeaed73f4e4156493eafbe7fb08
aldi jakaria <airakajidla@gmail.com>
2023-05-14 21:41:33 +0700

    change service provider

--- src/Providers/LaravelActivityLogProvider.php
+++ src/Providers/LaravelActivityLogProvider.php
@@ -16,13 +16,13 @@ class LaravelActivityLogProvider extends ServiceProvider
     public function register(): void
     {
         $this->app['router']->aliasMiddleware('track', TrackUserActivity::class);
-        $this->mergeConfigFrom(
-            __DIR__.'/../config/laravel-activity-log.php', 'laravel-activity-log'
-        );
     }
     public function boot(): void
     {
         $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
+        $this->mergeConfigFrom(
+            __DIR__.'/../config/laravel-activity-log.php', 'laravel-activity-log'
+        );
         $this->publishes([
             __DIR__.'/../config/laravel-activity-log.php' => config_path('laravel-activity-log.php'),
         ], 'config');
